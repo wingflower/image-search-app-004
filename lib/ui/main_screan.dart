0644fa365d16ad10e1cfb586e84237099866fbd2
Dongho Gwak <wing.flower.k@gmail.com>
2023-12-28 23:49:07 +0900

    fix: Mock repository 데이터 연결 및 테스트 완료

--- lib/ui/main_screan.dart
+++ lib/ui/main_screan.dart
@@ -1,24 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:image_search_app_004/data/repository/image_item_repository.dart';
 import 'package:image_search_app_004/ui/widget/image_item_widget.dart';
 
 import '../data/model/image_item.dart';
 
-class MainScreen extends StatelessWidget {
-  final double borderValue = 32;
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final searchTextEditingController = TextEditingController();
+
+  final repository = MockImageItemRepository();
+
+  static const double borderValue = 32;
+  List<ImageItem> imageItems = [];
+
+  Future<void> searchImage(String query) async {
+    imageItems = await repository.getImageItems(query);
+    setState(() {
+
+    });
+  }
+
+  @override
+  void dispose() {
+    searchTextEditingController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(32.0),
+          padding: const EdgeInsets.all(16),
           child: Column(
             children: [
               TextField(
+                controller: searchTextEditingController,
                 decoration: InputDecoration(
                   enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+                    borderRadius: BorderRadius.circular(16),
                     borderSide: BorderSide(
                       width: 2,
                       color: Color(0xFF4FB6B2),
@@ -28,25 +54,24 @@ class MainScreen extends StatelessWidget {
                   suffixIcon: IconButton(
                     icon: Icon(Icons.search),
                     color: Color(0xFF4FB6B2),
-                    onPressed: () {},
+                    onPressed: () {
+                      searchImage(searchTextEditingController.text);
+                    },
                   ),
                 ),
               ),
-              SizedBox(height: 32),
+              const SizedBox(height: 16),
               Expanded(
                 child: GridView.builder(
+                  itemCount: imageItems.length,
                   itemBuilder: (context, index) {
-                    final imageItem = ImageItem(
-                      imageUrl:
-                          'https://cdn.pixabay.com/photo/2017/09/26/13/21/apples-2788599_150.jpg',
-                      tags: 'apple',
-                    );
-                    return ImageItemWidget(borderValue: borderValue, imageItem: imageItem);
+                    final imageItem = imageItems[index];
+                    return ImageItemWidget(imageItem: imageItem);
                   },
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2,
-                    crossAxisSpacing: 32,
-                    mainAxisSpacing: 32,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
                   ),
                 ),
               ),
--- lib/ui/widget/image_item_widget.dart
+++ lib/ui/widget/image_item_widget.dart
@@ -3,19 +3,17 @@ import 'package:flutter/material.dart';
 import '../../data/model/image_item.dart';
 
 class ImageItemWidget extends StatelessWidget {
-  final double borderValue;
   final ImageItem imageItem;
 
   const ImageItemWidget({
     super.key,
-    required this.borderValue,
     required this.imageItem,
   });
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(borderValue),
+      borderRadius: BorderRadius.circular(16),
       child: Image.network(
         imageItem.imageUrl,
         fit: BoxFit.cover,
